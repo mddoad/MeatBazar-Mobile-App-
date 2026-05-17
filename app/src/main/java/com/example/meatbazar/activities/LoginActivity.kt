d4a64d0271551dc33725d5d294f068e6e2d71b1c
MD Doad <doadmd@gmail.com>
2026-05-18 00:53:47 +0600

    fixed AndroidMainfest.xml problem, temporary done SplashActivity.kt, and xml,loginActivit kt and xml, Dashboard Activit kt, adminDashboard kt, DistributorDashboardActivity kt

--- app/src/main/java/com/example/meatbazar/activities/LoginActivity.kt
+++ app/src/main/java/com/example/meatbazar/activities/LoginActivity.kt
@@ -1,4 +1,14 @@
 package com.example.meatbazar.activities
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.meatbazar.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
 }
--- app/src/main/java/com/example/meatbazar/distributor/DistributorDashboardActivity.kt
+++ app/src/main/java/com/example/meatbazar/distributor/DistributorDashboardActivity.kt
@@ -1,4 +1,11 @@
 package com.example.meatbazar.distributor
 
-class DistributorDashboardActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DistributorDashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
