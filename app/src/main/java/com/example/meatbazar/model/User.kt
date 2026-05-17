8e25d7ff4774a29f41ab667263d6ea0261ff9d36
MD Doad <doadmd@gmail.com>
2026-05-18 01:01:30 +0600

    Update user.kt and activity_register.xml

--- app/src/main/java/com/example/meatbazar/model/User.kt
+++ app/src/main/java/com/example/meatbazar/model/User.kt
@@ -1,10 +1,13 @@
-package com.example.meatbazar.model
+package com.example.meatbazar.models
 
 data class User(
+
     var uid: String = "",
+
     var name: String = "",
+
     var email: String = "",
-    var phone: String = "",
-    var address: String = "",
+
     var role: String = "USER"
+
 )
