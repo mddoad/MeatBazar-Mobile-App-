8a4d4cc9f0c55306a229cb1ef4a76e246fe772a4
MD Doad <doadmd@gmail.com>
2026-05-18 01:09:40 +0600

    Update login xml (add register option) and update login activity kt

--- app/src/main/java/com/example/meatbazar/activities/LoginActivity.kt
+++ app/src/main/java/com/example/meatbazar/activities/LoginActivity.kt
@@ -1,14 +1,32 @@
 package com.example.meatbazar.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.meatbazar.R
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        val registerNow =
+            findViewById<TextView>(R.id.registerNow)
+
+        registerNow.setOnClickListener {
+
+            startActivity(
+                Intent(
+                    this,
+                    RegisterActivity::class.java
+                )
+            )
+
+        }
+
     }
 }
