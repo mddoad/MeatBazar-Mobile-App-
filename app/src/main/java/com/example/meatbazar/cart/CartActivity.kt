2ddc44c97f7e77691a90630a214612bb3d53ecb4
MD Doad <doadmd@gmail.com>
2026-05-18 17:00:11 +0600

    Create CartActivity.kt and CheckoutActivity.kt

---
+++ app/src/main/java/com/example/meatbazar/cart/CartActivity.kt
@@ -0,0 +1,13 @@
+package com.example.meatbazar.cart
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.meatbazar.R
+
+class CartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
