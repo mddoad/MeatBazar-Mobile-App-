e78ee935e2111257caf08f4fa0590d5f86d3be75
MD Doad <doadmd@gmail.com>
2026-05-18 18:17:21 +0600

    update admin dashboard xml and activity

--- app/src/main/java/com/example/meatbazar/Dashboard/AdminDashboardActivity.kt
+++ app/src/main/java/com/example/meatbazar/Dashboard/AdminDashboardActivity.kt
@@ -1,13 +1,48 @@
 package com.example.meatbazar.dashboard
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.meatbazar.R
+import com.example.meatbazar.auth.LoginActivity
+import com.example.meatbazar.product.AddProductActivity
+import com.example.meatbazar.product.ProductListActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class AdminDashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_dashboard)
+
+        val btnAddProduct = findViewById<Button>(R.id.btnAddProduct)
+        val btnViewProducts = findViewById<Button>(R.id.btnViewProducts)
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
+
+        btnAddProduct.setOnClickListener {
+
+            startActivity(
+                Intent(this, AddProductActivity::class.java)
+            )
+        }
+
+        btnViewProducts.setOnClickListener {
+
+            startActivity(
+                Intent(this, ProductListActivity::class.java)
+            )
+        }
+
+        btnLogout.setOnClickListener {
+
+            FirebaseAuth.getInstance().signOut()
+
+            startActivity(
+                Intent(this, LoginActivity::class.java)
+            )
+
+            finish()
+        }
     }
 }
