a876c2a3cd46145edc400014266d28e41aca672c
MD Doad <doadmd@gmail.com>
2026-05-18 18:20:28 +0600

    add loagout button on customer dashboard

--- app/src/main/java/com/example/meatbazar/Dashboard/CustomerDashboardActivity.kt
+++ app/src/main/java/com/example/meatbazar/Dashboard/CustomerDashboardActivity.kt
@@ -2,13 +2,15 @@ package com.example.meatbazar.dashboard
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.meatbazar.R
 import com.example.meatbazar.adapter.ProductAdapter
-import com.example.meatbazar.cart.CartActivity
+import com.example.meatbazar.auth.LoginActivity
 import com.example.meatbazar.model.Product
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class CustomerDashboardActivity : AppCompatActivity() {
@@ -21,6 +23,8 @@ class CustomerDashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_dashboard)
 
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
+
         recyclerView = findViewById(R.id.recyclerProducts)
 
         productList = arrayListOf()
@@ -31,6 +35,17 @@ class CustomerDashboardActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
 
         loadProducts()
+
+        btnLogout.setOnClickListener {
+
+            FirebaseAuth.getInstance().signOut()
+
+            startActivity(
+                Intent(this, LoginActivity::class.java)
+            )
+
+            finish()
+        }
     }
 
     private fun loadProducts() {
