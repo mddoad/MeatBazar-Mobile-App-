e3c2be515b71ca62d7fec6a8474619e296817ee1
MD Doad <doadmd@gmail.com>
2026-05-18 18:41:47 +0600

    Create Admin order xml and activity kt

---
+++ app/src/main/java/com/example/meatbazar/order/AdminOrdersActivity.kt
@@ -0,0 +1,13 @@
+package com.example.meatbazar.order
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.meatbazar.R
+
+class AdminOrdersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_orders)
+    }
+}
